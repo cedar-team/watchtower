612be3e9b7aa781ccce7b3aebb51bcbdd546da51
Juan Gonzalez <jrg2156@gmail.com>
2021-03-09 18:41:14 -0700

    Tag as 1.0.6+cedar

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="watchtower",
-    version="1.0.6",
+    version="1.0.6+cedar",
     url="https://github.com/kislyuk/watchtower",
     license="Apache Software License",
     author="Andrey Kislyuk",
